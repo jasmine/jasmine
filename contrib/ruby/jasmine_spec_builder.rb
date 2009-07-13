b753cf64d653018f22fff224ce6b018c8911cf12
Kim Eric Mittler & Christian Williams <eng@emimusic.com>
2009-07-13 13:20:09 -0700

    Rename Jasmine::RemoteRunner to Jasmine::SpecBuilder.

--- contrib/ruby/jasmine_spec_builder.rb
+++ contrib/ruby/jasmine_spec_builder.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), "jasmine_runner.rb"))
 
 module Jasmine
 
-  class RemoteResults
+  class SpecBuilder
     attr_accessor :suites
 
     def initialize(spec_files, runner)
