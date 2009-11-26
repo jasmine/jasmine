3891b8afe4800c015398b8aef768e90acd810170
ragaskar <ragaskar@gmail.com>
2009-11-25 18:47:22 -0800

    Load JasmineHelper overrides in jasmine_meta_spec

--- lib/jasmine-ruby/jasmine_meta_spec.rb
+++ lib/jasmine-ruby/jasmine_meta_spec.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require "selenium_rc"
 require File.expand_path(File.join(File.dirname(__FILE__), "jasmine_helper.rb"))
+helper_overrides = File.expand_path(File.join(Dir.pwd, "spec", "helpers", "jasmine_helper.rb"))
+if File.exist?(helper_overrides)
+  require helper_overrides
+end
 require File.expand_path(File.join(File.dirname(__FILE__), "jasmine_runner.rb"))
 require File.expand_path(File.join(File.dirname(__FILE__), "jasmine_spec_builder"))
 
@@ -8,8 +12,8 @@ jasmine_runner = Jasmine::Runner.new(SeleniumRC::Server.new.jar_path,
                                      Dir.pwd,
                                      JasmineHelper.specs,
                                      { :spec_helpers => JasmineHelper.files + JasmineHelper.spec_helpers,
-                                       :stylesheets => JasmineHelper.stylesheets 
-                                       })
+                                       :stylesheets => JasmineHelper.stylesheets
+                                     })
 
 spec_builder = Jasmine::SpecBuilder.new(JasmineHelper.spec_files, jasmine_runner)
 
