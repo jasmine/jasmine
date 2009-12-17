849a4efda88fdb908c02529a2211d1d21be81f83
Nate Clark <nate@pivotallabs.com>
2009-12-16 17:27:06 -0800

    adds support to run Jasmine suites on CI using Sauce Labs

--- spec/jasmine_spec.rb
+++ spec/jasmine_spec.rb
@@ -1,11 +1,20 @@
 require 'rubygems'
-require "selenium_rc"
 require File.expand_path(File.join(File.dirname(__FILE__), "jasmine_helper.rb"))
 require File.expand_path(File.join(JasmineHelper.jasmine_root, "contrib/ruby/jasmine_spec_builder"))
 
-jasmine_runner = Jasmine::Runner.new(SeleniumRC::Server.new.jar_path,
-                                     JasmineHelper.specs,
-                                     JasmineHelper.dir_mappings)
+jasmine_runner = if ENV['SAUCELABS'] == 'true'
+  require 'sauce_tunnel'
+  require 'selenium_config'
+  Jasmine::SauceLabsRunner.new(JasmineHelper.specs,
+    JasmineHelper.dir_mappings,
+    :saucelabs_config => 'saucelabs',
+    :saucelabs_config_file => File.expand_path(File.join(File.dirname(__FILE__), "saucelabs.yml")))
+else
+  require "selenium_rc"
+  Jasmine::Runner.new(SeleniumRC::Server.new.jar_path,
+    JasmineHelper.specs,
+    JasmineHelper.dir_mappings)
+end
 
 spec_builder = Jasmine::SpecBuilder.new(JasmineHelper.raw_spec_files, jasmine_runner)
 
