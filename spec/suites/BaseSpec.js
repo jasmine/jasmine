1154fcaf3b4f2f5b0c0197dbdd0f5ab0280499e0
Lee Byrd & Christian Williams <ops+jasmine@pivotallabs.com>
2010-06-22 16:45:35 -0700

    Refactor.

--- spec/suites/BaseSpec.js
+++ spec/suites/BaseSpec.js
@@ -1,25 +1,27 @@
-describe("jasmine.MessageResult", function() {
-  it("#toString should pretty-print and concatenate each part of the message", function() {
-    var values = ["log", "message", 123, {key: "value"}, "FTW!"];
-    var messageResult = new jasmine.MessageResult(values);
-    expect(messageResult.toString()).toEqual("log message 123 { key : 'value' } FTW!");
-  });  
-});
-
-describe("jasmine.log", function() {
-  it("should accept n arguments", function() {
-    spyOn(jasmine.getEnv().currentSpec, 'log');
-    jasmine.log(1, 2, 3);
-    expect(jasmine.getEnv().currentSpec.log).wasCalledWith(1, 2, 3);
+describe("base.js", function() {
+  describe("jasmine.MessageResult", function() {
+    it("#toString should pretty-print and concatenate each part of the message", function() {
+      var values = ["log", "message", 123, {key: "value"}, "FTW!"];
+      var messageResult = new jasmine.MessageResult(values);
+      expect(messageResult.toString()).toEqual("log message 123 { key : 'value' } FTW!");
+    });
   });
-});
 
-describe("jasmine.getGlobal", function() {
-  it("should return the global object", function() {
-    var globalObject = (function() {
-      return this;
-    })();
+  describe("jasmine.log", function() {
+    it("should accept n arguments", function() {
+      spyOn(jasmine.getEnv().currentSpec, 'log');
+      jasmine.log(1, 2, 3);
+      expect(jasmine.getEnv().currentSpec.log).wasCalledWith(1, 2, 3);
+    });
+  });
 
-    expect(jasmine.getGlobal()).toBe(globalObject);
+  describe("jasmine.getGlobal", function() {
+    it("should return the global object", function() {
+      var globalObject = (function() {
+        return this;
+      })();
+
+      expect(jasmine.getGlobal()).toBe(globalObject);
+    });
   });
 });
