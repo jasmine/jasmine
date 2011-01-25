a5b292f59b4ea160daff23d5f616abf335fdb1c2
Davis W. Frank <dwfrank@infe.ws>
2011-01-25 13:13:32 -0800

    Move to staticmatic generated static HTML instead of relying on Jekyll (or server-side Jekyll)

---
+++ src/helpers/snippet_helper.rb
@@ -0,0 +1,9 @@
+module SnippetHelper
+  def snippet
+    "<span class='nx'>describe</span><span class='p'>(</span><span class='s2'>&quot;Jasmine&quot;</span><span class='p'>,</span> <span class='kd'>function</span><span class='p'>()</span> <span class='p'>{</span>
+  <span class='nx'>it</span><span class='p'>(</span><span class='s2'>&quot;makes testing JavaScript awesome!&quot;</span><span class='p'>,</span> <span class='kd'>function</span><span class='p'>()</span> <span class='p'>{</span>
+    <span class='nx'>expect</span><span class='p'>(</span><span class='nx'>yourCode</span><span class='p'>).</span><span class='nx'>toBeLotsBetter</span><span class='p'>();</span>
+  <span class='p'>});</span>
+<span class='p'>});</span>"
+  end
+end
