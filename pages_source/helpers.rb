209f74f835dab9f13b8ed140a0fedbda002c0d48
Davis W. Frank <dwfrank@infe.ws>
2011-06-17 08:19:36 -0700

    New JSDoc for 1.1.0.rc1; small fixes to pages

--- pages_source/helpers.rb
+++ pages_source/helpers.rb
@@ -1,10 +1,12 @@
 module FrankHelpers
   def jasmine_snippet
-    "<span class='nx'>describe</span><span class='p'>(</span><span class='s2'>&quot;Jasmine&quot;</span><span class='p'>,</span> <span class='kd'>function</span><span class='p'>()</span> <span class='p'>{</span>
+    <<HTML
+<span class='nx'>describe</span><span class='p'>(</span><span class='s2'>&quot;Jasmine&quot;</span><span class='p'>,</span> <span class='kd'>function</span><span class='p'>()</span> <span class='p'>{</span>
   <span class='nx'>it</span><span class='p'>(</span><span class='s2'>&quot;makes testing JavaScript awesome!&quot;</span><span class='p'>,</span> <span class='kd'>function</span><span class='p'>()</span> <span class='p'>{</span>
     <span class='nx'>expect</span><span class='p'>(</span><span class='nx'>yourCode</span><span class='p'>).</span><span class='nx'>toBeLotsBetter</span><span class='p'>();</span>
   <span class='p'>});</span>
-<span class='p'>});</span>"
+<span class='p'>});</span>
+HTML
   end
 
   def downloads
@@ -20,6 +22,12 @@ module FrankHelpers
       item[:sha]      = Digest::SHA1.hexdigest File.read(f)
       item
     end
+  end
+
+  def link(href)
+    attrs = { :href => href }
+    attrs[:target => '_blank' ] unless attrs.match /^mailto/
+    attrs
   end
 
 end
