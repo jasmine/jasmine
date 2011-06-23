b722f416c78a84787d6a44810d8916518c222042
ragaskar <ragaskar@gmail.com>
2011-06-23 07:55:04 -0400

    Write out version.rb on dist build

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source :rubygems
 gem "term-ansicolor", :require => "term/ansicolor"
-gem "json_pure", ">= 1.4.3"
 gemspec
--- src/version.js
+++ src/version.js
@@ -2,6 +2,6 @@ jasmine.version_= {
   "major": 1,
   "minor": 1,
   "build": 0,
-  "revision": 1308618948,
+  "revision": 1308830021,
   "release_candidate": 1
 };
