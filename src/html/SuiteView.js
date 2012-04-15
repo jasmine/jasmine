1771ec3c633fdc91ae8a32890cb352f5a79bc471
Alex Kwiatkowski <alex+git@rival-studios.com>
2012-04-15 17:45:50 -0400

    Add checkbox to test runner which toggles catching of exceptions during tests

--- src/html/SuiteView.js
+++ src/html/SuiteView.js
@@ -4,7 +4,7 @@ jasmine.HtmlReporter.SuiteView = function(suite, dom, views) {
   this.views = views;
 
   this.element = this.createDom('div', { className: 'suite' },
-      this.createDom('a', { className: 'description', href: '?spec=' + encodeURIComponent(this.suite.getFullName()) }, this.suite.description)
+    this.createDom('a', { className: 'description', href: jasmine.HtmlReporter.sectionLink(this.suite.getFullName()) }, this.suite.description)
   );
 
   this.appendToSummary(this.suite, this.element);
