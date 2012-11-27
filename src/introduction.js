00ba05c213c22325e2dfc21307a8d07a379fd9a4
Davis W. Frank & Rajan Agaskar <pair+dwfrank+rajan@pivotallabs.com>
2012-11-27 13:45:55 -0800

    Releasing for 1.3

--- src/introduction.js
+++ src/introduction.js
@@ -173,7 +173,7 @@ describe("Included matchers:", function() {
   it("The 'toBeCloseTo' matcher is for precision math comparison", function() {
     var pi = 3.1415926, e = 2.78;
 
-    expect(pi).not.toBeCloseTo(e, 0.1);
+    expect(pi).not.toBeCloseTo(e, 2);
     expect(pi).toBeCloseTo(e, 0);
   });
 
