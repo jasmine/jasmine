f68657f14e46bdaa144c24f457a8501a21b90d32
Sheel Choksi <sheelchoksi@gmail.com>
2013-06-16 22:10:31 -0700

    Have Jasmine HTML use the source files in specs
    Similar to the changes in Jasmine core and console, this gets the
    HTML specs of Jasmine using j$ instead of jasmine so that they use
    the source files instead of the built distribution

--- spec/html/PrettyPrintHtmlSpec.js
+++ spec/html/PrettyPrintHtmlSpec.js
@@ -1,8 +1,8 @@
-describe("jasmine.pp (HTML Dependent)", function () {
+describe("j$.pp (HTML Dependent)", function () {
   it("should stringify HTML nodes properly", function() {
     var sampleNode = document.createElement('div');
     sampleNode.innerHTML = 'foo<b>bar</b>';
-    expect(jasmine.pp(sampleNode)).toEqual("HTMLNode");
-    expect(jasmine.pp({foo: sampleNode})).toEqual("{ foo : HTMLNode }");
+    expect(j$.pp(sampleNode)).toEqual("HTMLNode");
+    expect(j$.pp({foo: sampleNode})).toEqual("{ foo : HTMLNode }");
   });
 });
