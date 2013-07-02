d0b28b94d9aedebc8e56c585a0227e07e2b29d23
Jason <jdmarshall@gmail.com>
2013-07-02 00:09:50 -0700

    Change tests to use the right copy of jasmine

--- spec/html/PrettyPrintHtmlSpec.js
+++ spec/html/PrettyPrintHtmlSpec.js
@@ -2,7 +2,7 @@ describe("jasmine.pp (HTML Dependent)", function () {
   it("should stringify HTML nodes properly", function() {
     var sampleNode = document.createElement('div');
     sampleNode.innerHTML = 'foo<b>bar</b>';
-    expect(jasmine.pp(sampleNode)).toEqual("HTMLNode");
-    expect(jasmine.pp({foo: sampleNode})).toEqual("{ foo : HTMLNode }");
+    expect(j$.pp(sampleNode)).toEqual("HTMLNode");
+    expect(j$.pp({foo: sampleNode})).toEqual("{ foo : HTMLNode }");
   });
 });
