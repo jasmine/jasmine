aa61929f0059d8a34241f476c4dfe7f60bd0e4fe
Greg Cobb and JR Boyens <pair+gcobb+jboyens@pivotallabs.com>
2013-07-15 15:45:14 -0700

    Unattended things should not ask questions

--- travis-python-setup.sh
+++ travis-python-setup.sh
@@ -1,4 +1,4 @@
-add-apt-repository ppa:fkrull/deadsnakes
+add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
 apt-get install python2.6 python2.6-dev python3.3 python3.3-dev pypy pypy-dev
 pip install tox
