8d295a261212e15f3a21fc0f46601ac03e16da21
JR Boyens <pair+jboyens@pivotallabs.com>
2013-07-26 12:35:24 -0700

    Move more things out to the core_script

--- travis-core-script.sh
+++ travis-core-script.sh
@@ -2,4 +2,11 @@
 
 export DISPLAY=:99.0
 
+/etc/init.d/xvfb start
+
+if [ $USE_SAUCE == true ]
+then
+    curl https://gist.github.com/santiycr/5139565/raw/sauce_connect_setup.sh | bash
+fi
+
 bundle exec rake core_spec
