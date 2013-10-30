cb5aea1fcff3f5475cca0e591078cb0ade440171
Sheel Choksi <sheelchoksi@gmail.com>
2013-10-29 17:32:04 -0700

    Fix builds by working around browser timing functions not being successfully overridden

--- spec/support/dev_boot.js
+++ spec/support/dev_boot.js
@@ -94,6 +94,11 @@
     return specFilter.matches(spec.getFullName());
   };
 
+  window.setTimeout = window.setTimeout;
+  window.setInterval = window.setInterval;
+  window.clearTimeout = window.clearTimeout;
+  window.clearInterval = window.clearInterval;
+
   var currentWindowOnload = window.onload;
 
   window.onload = function() {
