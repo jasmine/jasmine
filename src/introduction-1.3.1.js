a21671ed7129fff75f5830adedb1a790f4ad5438
Sheel Choksi <sheelchoksi@gmail.com>
2013-12-17 12:12:27 -0800

    Updating standalone distribution links to new location
    Inconsistency found by @nfriedly

--- src/introduction-1.3.1.js
+++ src/introduction-1.3.1.js
@@ -708,7 +708,7 @@ describe("Asynchronous specs", function() {
 
 // ## Downloads
 //
-// * The [Standalone Release](http://github.com/pivotal/jasmine/downloads) is for simple, browser page, or console projects
+// * The [Standalone Release](https://github.com/pivotal/jasmine/tree/master/dist) is for simple, browser page, or console projects
 // * The [Jasmine Ruby Gem](http://github.com/pivotal/jasmine-gem) is for Rails, Ruby, or Ruby-friendly development
 // * [Other Environments](http://github.com/pivotal/jasmine/wiki) are supported as well
 //
