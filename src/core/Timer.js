b5775aec4f4435ffbbfe9bd50e4b2c1e4d1bc57f
tcorral <amischol@gmail.com>
2014-01-24 02:02:23 +0100

    Allow stub or spy Date object safely using a closure to get a clean copy
    [closes #506]

--- src/core/Timer.js
+++ src/core/Timer.js
@@ -1,9 +1,13 @@
 getJasmineRequireObj().Timer = function() {
+  var defaultNow = (function(Date) {
+    return function() { return new Date().getTime(); };
+  })(Date);
+
   function Timer(options) {
     options = options || {};
 
-    var now = options.now || function() { return new Date().getTime(); },
-        startTime;
+    var now = options.now || defaultNow,
+      startTime;
 
     this.start = function() {
       startTime = now();
