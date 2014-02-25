84160ff51dcdda4449b0081594f809c458ec6acb
Charles Hansen and Gregg Van Hove <pair+chansen+gvanhove@pivotallabs.com>
2014-02-25 11:28:02 -0800

    revert to old version of checking the docs

--- travis-docs-script.sh
+++ travis-docs-script.sh
@@ -2,10 +2,4 @@
 
 git clone https://github.com/jasmine/jasmine.github.io.git
 
-mkdir -p jasmine.github.io/edge/lib
-cp lib/jasmine-core/{boot.js,jasmine-html.js,jasmine.js,jasmine.css} jasmine.github.io/edge/lib/
-cp lib/console/console.js jasmine.github.io/edge/lib/
-
-cd jasmine.github.io
-bundle install
-JASMINE_VERSION=edge bundle exec rake phantom
+bundle exec rake jasmine:ci JASMINE_CONFIG_PATH=jasmine.github.io/edge/spec/support/jasmine.yml
