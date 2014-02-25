23c0e379e0cb11d4eb3e313dd282736f1fad2881
Charles Hansen and Gregg Van Hove <pair+chansen+gvanhove@pivotallabs.com>
2014-02-25 10:42:59 -0800

    update edge docs when changes are made to jasmine

--- travis-docs-script.sh
+++ travis-docs-script.sh
@@ -1,3 +1,18 @@
+#!/bin/bash -e
+
 git clone https://github.com/jasmine/jasmine.github.io.git
 
-bundle exec rake jasmine:ci JASMINE_CONFIG_PATH=jasmine.github.io/2.0/spec/support/jasmine.yml
+mkdir -p jasmine.github.io/edge/lib
+cp lib/jasmine-core/{boot.js,jasmine-html.js,jasmine.js,jasmine.css} jasmine.github.io/edge/lib/
+cp lib/console/console.js jasmine.github.io/edge/lib/
+
+cd jasmine.github.io
+export JASMINE_VERSION=edge
+CODE=`bundle exec rake phantom`
+
+if [ $TRAVIS_PULL_REQUEST == false ]
+then
+  git push origin master
+fi
+
+exit $CODE
