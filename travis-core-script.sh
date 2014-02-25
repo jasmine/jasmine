095b02ad8355d5bda2f7c74246c9f2eed4104698
Greg Cobb and Sheel Choksi <pair+gcobb+schoksi@pivotallabs.com>
2014-02-24 16:13:25 -0800

    Follow redirects when trying to connect to sauce

--- travis-core-script.sh
+++ travis-core-script.sh
@@ -4,7 +4,7 @@ if [ $USE_SAUCE == true ]
 then
   if [ $TRAVIS_SECURE_ENV_VARS == true ]
   then
-    curl https://gist.github.com/santiycr/5139565/raw/sauce_connect_setup.sh | bash
+    curl -L https://gist.github.com/santiycr/5139565/raw/sauce_connect_setup.sh | bash
   else
     echo "skipping tests since we can't use sauce"
     exit 0
