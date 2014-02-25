32a666ffe05b0b3e3daf1090914f67e47123c5b8
Charles Hansen and Gregg Van Hove <pair+chansen+gvanhove@pivotallabs.com>
2014-02-25 14:20:10 -0800

    remove old docs. All docs now at jasmine.github.io

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'rocco', git: 'https://github.com/infews/rocco.git' # to get around a 14+-month-old bug in Rocco
-gem 'tilt'
--- Gruntfile.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (grunt) {
-  grunt.initConfig({
-    jshint: require('./grunt/config/jshint.js')
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', ['jshint:all']);
-};
