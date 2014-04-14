b2bbbc43ec0b0a670afd115e3b984646d44b15ea
Alex Treppass <alextreppass@gmail.com>
2014-04-14 14:23:50 +0100

    keeping track of passed expectations, and the total number of expectations

--- spec/html/HtmlReporterSpec.js
+++ spec/html/HtmlReporterSpec.js
@@ -148,7 +148,9 @@ describe("New HtmlReporter", function() {
       reporter.specDone({
         id: 345,
         status: "failed",
-        failedExpectations: []
+        failedExpectations: [],
+        passedExpectations: [],
+        totalExpectations: 0
       });
 
       var specEl = container.querySelector(".symbol-summary li");
@@ -272,7 +274,9 @@ describe("New HtmlReporter", function() {
         description: "with a failing spec",
         fullName: "A Suite inner with a failing spec",
         status: "failed",
-        failedExpectations: []
+        failedExpectations: [],
+        passedExpectations: [],
+        totalExpectations: 0
       };
       reporter.specStarted(specResult);
       reporter.specDone(specResult);
