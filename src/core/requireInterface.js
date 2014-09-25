15ae0379ec862ea7f9608d9b57bafb302263e2a9
slackersoft <gregg@slackersoft.net>
2014-09-25 13:19:13 -0700

    Add `fit` and `fdescribe` to the public interface
    They got lost in the merge

--- src/core/requireInterface.js
+++ src/core/requireInterface.js
@@ -8,12 +8,20 @@ getJasmineRequireObj().interface = function(jasmine, env) {
       return env.xdescribe(description, specDefinitions);
     },
 
+    fdescribe: function(description, specDefinitions) {
+      return env.fdescribe(description, specDefinitions);
+    },
+
     it: function(desc, func) {
       return env.it(desc, func);
     },
 
     xit: function(desc, func) {
       return env.xit(desc, func);
+    },
+
+    fit: function(desc, func) {
+      return env.fit(desc, func);
     },
 
     beforeEach: function(beforeEachFunction) {
