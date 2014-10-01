cfc64a6f5ccf5db9b74f3adfcee7ceef606aaab7
slackersoft <gregg@slackersoft.net>
2014-10-01 09:59:39 -0700

    bundle exec the docs rake tasks

--- travis-docs-script.sh
+++ travis-docs-script.sh
@@ -5,5 +5,5 @@ git clone https://github.com/jasmine/jasmine.github.io.git
 cd jasmine.github.io
 bundle
 
-rake update_edge_jasmine
-rake phantom
+bundle exec rake update_edge_jasmine
+bundle exec rake phantom
