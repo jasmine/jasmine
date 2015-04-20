e4020c3bdabc3f51975a84d306451aeb867dcde9
Ben Lakey <benlakey@gmail.com>
2015-04-19 17:02:46 -0700

    Alias toBeFalsy/toBeFalsey to allow for both wording styles.

--- spec/core/matchers/toBeFalsySpec.js
+++ spec/core/matchers/toBeFalsySpec.js
@@ -1,38 +1,46 @@
 describe("toBeFalsy", function() {
-  it("passes for 'falsy' values", function() {
-    var matcher = j$.matchers.toBeFalsy(),
-      result;
 
-    result = matcher.compare(false);
-    expect(result.pass).toBe(true);
+  function test(matcher) {
+    it("passes for 'falsy' values", function() {
+      var result;
 
-    result = matcher.compare(0);
-    expect(result.pass).toBe(true);
+      result = matcher.compare(false);
+      expect(result.pass).toBe(true);
 
-    result = matcher.compare('');
-    expect(result.pass).toBe(true);
+      result = matcher.compare(0);
+      expect(result.pass).toBe(true);
 
-    result = matcher.compare(null);
-    expect(result.pass).toBe(true);
+      result = matcher.compare('');
+      expect(result.pass).toBe(true);
 
-    result = matcher.compare(void 0);
-    expect(result.pass).toBe(true);
+      result = matcher.compare(null);
+      expect(result.pass).toBe(true);
+
+      result = matcher.compare(void 0);
+      expect(result.pass).toBe(true);
+    });
+
+    it("fails for 'truthy' values", function() {
+      var result;
+
+      result = matcher.compare(true);
+      expect(result.pass).toBe(false);
+
+      result = matcher.compare(1);
+      expect(result.pass).toBe(false);
+
+      result = matcher.compare("foo");
+      expect(result.pass).toBe(false);
+
+      result = matcher.compare({});
+      expect(result.pass).toBe(false);
+    });
+  }
+
+  test(j$.matchers.toBeFalsy());
+
+  describe('with toBeFalsey alias', function() {
+    test(j$.matchers.toBeFalsey());
   });
 
-  it("fails for 'truthy' values", function() {
-    var matcher = j$.matchers.toBeFalsy(),
-      result;
-
-    result = matcher.compare(true);
-    expect(result.pass).toBe(false);
-
-    result = matcher.compare(1);
-    expect(result.pass).toBe(false);
-
-    result = matcher.compare("foo");
-    expect(result.pass).toBe(false);
-
-    result = matcher.compare({});
-    expect(result.pass).toBe(false);
-  });
 });
