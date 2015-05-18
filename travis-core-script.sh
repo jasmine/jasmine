4be20794827a63ca5295848c58ffc478624ee4dc
Gregg Van Hove and Noah Denton <pair+gvanhove+ndenton@pivotallabs.com>
2015-05-18 16:55:44 -0700

    Use the travis sauce connect addon

--- travis-core-script.sh
+++ travis-core-script.sh
@@ -2,10 +2,8 @@
 
 if [ $USE_SAUCE == true ]
 then
-  if [ $TRAVIS_SECURE_ENV_VARS == true ]
+  if [ $TRAVIS_SECURE_ENV_VARS != true ]
   then
-    curl -L https://gist.github.com/santiycr/5139565/raw/sauce_connect_setup.sh | bash
-  else
     echo "skipping tests since we can't use sauce"
     exit 0
   fi
