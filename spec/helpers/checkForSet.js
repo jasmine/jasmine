5279e7a8c423b1fb3cda58a6b51056ed146e2984
Gregg Van Hove <gvanhove@pivotal.io>
2016-09-27 16:11:43 -0700

    Checking for Set isn't browser dependant

---
+++ spec/helpers/checkForSet.js
@@ -0,0 +1,21 @@
+(function(env) {
+  function hasFunctioningSets() {
+    if (typeof Set === 'undefined') { return false; }
+
+    try {
+      var s = new Set([4]);
+      if (s.size !== 1) { return false; }
+      if (s.values().next().value !== 4) { return false; }
+      return true;
+    } catch(e) {
+      return false;
+    }
+  }
+
+  env.requireFunctioningSets = function() {
+    if (!hasFunctioningSets()) {
+      pending("Browser has incomplete or missing support for Sets");
+    }
+  };
+
+})(jasmine.getEnv());
