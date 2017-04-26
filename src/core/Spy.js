2689a2a6f817ecca73aac8ace6c01df61b36c5a8
Tony Brix <Tony@Brix.ninja>
2017-04-26 00:26:15 -0500

    Revert "use Object.defineProperty"

--- src/core/Spy.js
+++ src/core/Spy.js
@@ -14,9 +14,10 @@ getJasmineRequireObj().Spy = function (j$) {
    * @name Spy
    */
   function Spy(name, originalFn) {
-    var wrapper = function () {
+    var numArgs = (typeof originalFn === 'function' ? originalFn.length : 0),
+      wrapper = makeFunc(numArgs, function () {
         return spy.apply(this, Array.prototype.slice.call(arguments));
-      },
+      }),
       spyStrategy = new j$.SpyStrategy({
         name: name,
         fn: originalFn,
@@ -45,7 +46,20 @@ getJasmineRequireObj().Spy = function (j$) {
         return returnValue;
       };
 
-    Object.defineProperty(wrapper, 'length', { value: (typeof originalFn === 'function' ? originalFn.length : 0) });
+    function makeFunc(length, fn) {
+      switch (length) {
+        case 1 : return function (a) { return fn.apply(this, arguments); };
+        case 2 : return function (a,b) { return fn.apply(this, arguments); };
+        case 3 : return function (a,b,c) { return fn.apply(this, arguments); };
+        case 4 : return function (a,b,c,d) { return fn.apply(this, arguments); };
+        case 5 : return function (a,b,c,d,e) { return fn.apply(this, arguments); };
+        case 6 : return function (a,b,c,d,e,f) { return fn.apply(this, arguments); };
+        case 7 : return function (a,b,c,d,e,f,g) { return fn.apply(this, arguments); };
+        case 8 : return function (a,b,c,d,e,f,g,h) { return fn.apply(this, arguments); };
+        case 9 : return function (a,b,c,d,e,f,g,h,i) { return fn.apply(this, arguments); };
+        default : return function () { return fn.apply(this, arguments); };
+      }
+    }
 
     for (var prop in originalFn) {
       if (prop === 'and' || prop === 'calls') {
