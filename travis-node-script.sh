56a79bef98db1f70d451fb247182231ff6de421b
Steve Gravrock <sgravrock@pivotal.io>
2017-05-19 15:43:01 -0700

    Use a less ancient Node on Travis CI

--- travis-node-script.sh
+++ travis-node-script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
 
+rm -rf ~/.nvm
+git clone https://github.com/creationix/nvm.git ~/.nvm
+(cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
+source ~/.nvm/nvm.sh
+nvm install v0.12.18
+
 npm install
 npm test
