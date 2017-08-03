75cb4e2b706b9e54cbff394bf154fd3a8fdc5715
ksvitkovsky <ksvitkovsky@yandex.ru>
2017-08-04 00:50:04 +0400

    Handle non-standard function format in fnNameFor
    Some browsers have non-standard toString presentation
    of standard class constructors (e.g. typed arrays in PhantomJS)

--- src/core/base.js
+++ src/core/base.js
@@ -92,7 +92,9 @@ getJasmineRequireObj().base = function(j$, jasmineGlobal) {
       return func.name;
     }
 
-    var matches = func.toString().match(/^\s*function\s*(\w*)\s*\(/);
+    var matches = func.toString().match(/^\s*function\s*(\w*)\s*\(/) ||
+      func.toString().match(/^\s*\[object\s*(\w*)Constructor\]/);
+
     return matches ? matches[1] : '<anonymous>';
   };
 
