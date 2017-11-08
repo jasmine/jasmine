676689b0633f6b6fd2f47ce09c498bc05f207303
Steve Gravrock <sgravrock@pivotal.io>
2017-11-08 09:11:41 -0800

    Test against Node 4.x, not 0.10.x

--- travis-node-script.sh
+++ travis-node-script.sh
@@ -4,7 +4,7 @@ rm -rf ~/.nvm
 git clone https://github.com/creationix/nvm.git ~/.nvm
 (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
 source ~/.nvm/nvm.sh
-nvm install v0.12.18
+nvm install v4.8.6
 
 npm install
 npm test
