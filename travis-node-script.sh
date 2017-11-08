d58f3dac5698505dda352ecdd4f06105688807d6
Steve Gravrock <sgravrock@pivotal.io>
2017-11-08 09:17:55 -0800

    Test against multiple Node versions on Travis

--- travis-node-script.sh
+++ travis-node-script.sh
@@ -4,7 +4,7 @@ rm -rf ~/.nvm
 git clone https://github.com/creationix/nvm.git ~/.nvm
 (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
 source ~/.nvm/nvm.sh
-nvm install v4.8.6
+nvm install $NODE_VERSION
 
 npm install
 npm test
