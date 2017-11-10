2eef0747a0301d3f355860c4ba2a4ce58239c173
Julian Lannigan <julian.lannigan@gmail.com>
2017-11-10 10:22:58 -0500

    Added test steps for other major node versions addresses https://www.pivotaltracker.com/story/show/152685778

--- travis-node-script.sh
+++ travis-node-script.sh
@@ -4,7 +4,7 @@ rm -rf ~/.nvm
 git clone https://github.com/creationix/nvm.git ~/.nvm
 (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
 source ~/.nvm/nvm.sh
-nvm install v0.12.18
+nvm install ${1:-"v0.12.18"}
 
 npm install
 npm test
