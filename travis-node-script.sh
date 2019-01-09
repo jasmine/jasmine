489fb79d6e8e0e4f6e6300737d252bc88e434584
Gregg Van Hove <gregg@slackersoft.net>
2019-01-09 11:08:32 -0800

    fall back to older jshint for older node

--- travis-node-script.sh
+++ travis-node-script.sh
@@ -6,5 +6,9 @@ git clone https://github.com/creationix/nvm.git ~/.nvm
 source ~/.nvm/nvm.sh
 nvm install ${1:-"v0.12.18"}
 
+if [ "$1" = "v4" ]; then
+  node -e "pack=require('./package.json');pack.devDependencies['grunt-contrib-jshint']='^1.1.0';require('fs').writeFileSync('./package.json', JSON.stringify(pack, null, 2));"
+fi
+
 npm install
 npm test
