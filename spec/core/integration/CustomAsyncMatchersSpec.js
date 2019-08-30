ef5b0848967d8ad53ef5fffe47110b43c7c97da4
Tony Brix <tony@brix.ninja>
2019-08-29 22:51:43 -0500

    add requirePromises() to integration specs

--- spec/core/integration/CustomAsyncMatchersSpec.js
+++ spec/core/integration/CustomAsyncMatchersSpec.js
@@ -7,6 +7,8 @@ describe('Custom Async Matchers (Integration)', function() {
   });
 
   it('passes the spec if the custom async matcher passes', function(done) {
+    jasmine.getEnv().requirePromises();
+
     env.it('spec using custom async matcher', function() {
       env.addAsyncMatchers({
         toBeReal: function() {
@@ -26,6 +28,8 @@ describe('Custom Async Matchers (Integration)', function() {
   });
 
   it('uses the negative compare function for a negative comparison, if provided', function(done) {
+    jasmine.getEnv().requirePromises();
+
     env.it('spec with custom negative comparison matcher', function() {
       env.addAsyncMatchers({
         toBeReal: function() {
@@ -48,6 +52,8 @@ describe('Custom Async Matchers (Integration)', function() {
   });
 
   it('generates messages with the same rules as built in matchers absent a custom message', function(done) {
+    jasmine.getEnv().requirePromises();
+
     env.it('spec with an expectation', function() {
       env.addAsyncMatchers({
         toBeReal: function() {
