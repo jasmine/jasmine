abe38429f5ee4e40727b8a2c22e36ebc9bf5b10e
Francois Wauquier <fwauquier@oui.sncf>
2020-03-12 18:33:41 +0100

    Simplification using 'isArrayLike' to detect when to use length

--- src/core/matchers/toHaveSize.js
+++ src/core/matchers/toHaveSize.js
@@ -20,25 +20,15 @@ getJasmineRequireObj().toHaveSize = function(j$) {
         // Avoid misleading collections size matching
         if (actual instanceof WeakSet
           || actual instanceof WeakMap
-          || actual instanceof DataView)
-          throw new Error('Cannot get size of ' + actual + '.');
+          || actual instanceof DataView) {
+          result.message = 'Cannot get size of ' + actual + '.';
+          return result;
+        }
 
         // Ref https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
-        if (Array.isArray(actual))
+        if (Array.isArray(actual) || isArrayLike(actual))
           result.pass = matchersUtil.equals(actual.length, expected, diffBuilder);
         else if ( actual instanceof String || typeof actual === 'string')
-          result.pass =  matchersUtil.equals(actual.length, expected, diffBuilder);
-        else if (actual.constructor.name === 'BigInt64Array'
-          || actual.constructor.name === 'BigUint64Array'
-          || actual.constructor.name === 'Float32Array'
-          || actual.constructor.name === 'Float64Array'
-          || actual.constructor.name === 'Int8Array'
-          || actual.constructor.name === 'Int16Array'
-          || actual.constructor.name === 'Int32Array'
-          || actual.constructor.name === 'Uint8Array'
-          || actual.constructor.name === 'Uint8ClampedArray'
-          || actual.constructor.name === 'Uint16Array'
-          || actual.constructor.name === 'Uint32Array')
           result.pass =  matchersUtil.equals(actual.length, expected, diffBuilder);
         else if (actual instanceof Set || actual instanceof Map)
           result.pass = matchersUtil.equals(actual.size, expected, diffBuilder);
@@ -51,6 +41,35 @@ getJasmineRequireObj().toHaveSize = function(j$) {
         return result;
       }
     };
+  }
+
+  /**
+   * Checks if `value` is array-like. A value is considered array-like if it's
+   * not a function and has a `value.length` that's an integer greater than or
+   * equal to `0` and less than or equal to `Number.MAX_SAFE_INTEGER`.
+   * From lodash
+   * @param {*} value The value to check.
+   * @returns {boolean} Returns `true` if `value` is array-like, else `false`.
+   * @example
+   * _.isArrayLike([1, 2, 3]);
+   * // => true
+   * _.isArrayLike(document.body.children);
+   * // => true
+   * _.isArrayLike('abc');
+   * // => true
+   * _.isArrayLike(_.noop);
+   * // => false
+   */
+  function isArrayLike(value) {
+    return value != null && isLength(value.length) && !isFunction(value);
+  }
+  var MAX_SAFE_INTEGER = 9007199254740991;
+  function isLength(value) {
+    return (typeof value == 'number') && value > -1 && value % 1 == 0 && value <= MAX_SAFE_INTEGER;
+  }
+  var functionTags = ['[object Function]','[object GeneratorFunction]','[object AsyncFunction]','[object Proxy]'];
+  function isFunction(functionToCheck) {
+    return functionToCheck && functionTags.indexOf( Object.prototype.toString.call(functionToCheck) ) != -1;
   }
 
   return toHaveSize;
