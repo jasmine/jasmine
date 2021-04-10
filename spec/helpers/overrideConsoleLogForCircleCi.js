a1177d441576625248b96cf5421796de720cb308
Steve Gravrock <sdg@panix.com>
2021-04-10 12:16:04 -0700

    make log output more visible

---
+++ spec/helpers/overrideConsoleLogForCircleCi.js
@@ -0,0 +1,18 @@
+(function() {
+  function override(fnName) {
+    var realFn = console[fnName];
+    console[fnName] = function() {
+      realFn.call(console, '\n');
+      realFn.apply(console, arguments);
+    }
+  }
+
+  if (process && process.env && process.env.CIRCLE_CI) {
+    // The Circle CI "terminal" is infinitely wide, which means log/error
+    // output will tend to be lost off to the right of all of the Jasmine
+    // reporter's dots. Prepend a newline to the output to ensure that it
+    // will be visible.
+    override('log');
+    override('error');
+  }
+})();
