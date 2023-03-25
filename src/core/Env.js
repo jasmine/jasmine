299fd1f770a9b495b5b4953ae2fd3b75bdc3fb47
Steve Gravrock <sdg@panix.com>
2023-03-25 11:21:04 -0700

    Removed unnecessary TODO

--- src/core/Env.js
+++ src/core/Env.js
@@ -509,7 +509,6 @@ getJasmineRequireObj().Env = function(j$) {
     };
 
     this.parallelReset = function() {
-      // TODO: ensure that autoCleanClosures was false
       suiteBuilder.parallelReset();
       runner.parallelReset();
     };
