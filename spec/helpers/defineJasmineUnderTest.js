2f0c4a6509acf08fcabcc34b10890ec5a2ddd516
Steve Gravrock <sdg@panix.com>
2025-09-27 09:46:16 -0700

    Fix and enable test

--- spec/helpers/defineJasmineUnderTest.js
+++ spec/helpers/defineJasmineUnderTest.js
@@ -2,7 +2,8 @@
   // By the time onload is called, jasmineRequire will be redefined to point
   // to the Jasmine source files (and not jasmine.js). So re-require
   window.jasmineUnderTest = jasmineRequire.core(jasmineRequire);
+  jasmineRequire.html(jasmineUnderTest);
+
   // Alias the private namespace so tests can be less verbose
   window.privateUnderTest = window.jasmineUnderTest.private;
-  jasmineRequire.html(jasmineUnderTest);
 })();
