5b3e12e4c56287078db931cb7d6d64d3a5948fd1
Steve Gravrock <sdg@panix.com>
2025-10-06 20:10:22 -0700

    Decompose HtmlReporter into smaller components

--- src/html/requireHtml.js
+++ src/html/requireHtml.js
@@ -3,6 +3,7 @@ var jasmineRequire = window.jasmineRequire || require('./jasmine.js');
 
 jasmineRequire.html = function(j$) {
   j$.private.ResultsNode = jasmineRequire.ResultsNode();
+  j$.private.DomContext = jasmineRequire.DomContext(j$);
   j$.HtmlReporter = jasmineRequire.HtmlReporter(j$);
   j$.QueryString = jasmineRequire.QueryString();
   j$.HtmlSpecFilter = jasmineRequire.HtmlSpecFilter();
